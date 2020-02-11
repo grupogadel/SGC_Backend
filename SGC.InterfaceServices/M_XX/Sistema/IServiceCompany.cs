84ac3bd72c31b592d297facfe70e9ab271e368cc
mauricio <mquirogalipe@gmail.com>
2020-02-11 16:38:26 -0500

    Files - Company

---
+++ SGC.InterfaceServices/M_XX/Sistema/IServiceCompany.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using SGC.Entities.Entities.M_XX.Sistema;
+
+namespace SGC.InterfaceServices.M_XX.Sistema
+{
+    public interface IServiceCompany
+    {
+        
+        Task<List<Company>> GetAll();
+        int Add(Company model);
+        int Update(Company model);
+        int Delete(int id);
+        Company Get(int id);
+    }
+}
