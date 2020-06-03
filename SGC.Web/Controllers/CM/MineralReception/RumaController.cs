503a4f1d207e483bbe28f92cea69f4a0394ac074
cristhianH <cristhian.dbest@gmail.com>
2020-06-03 10:56:32 -0500

    rodo updtae rumas

--- SGC.Web/Controllers/CM/MineralReception/RumaController.cs
+++ SGC.Web/Controllers/CM/MineralReception/RumaController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using SGC.Entities.Entities.CM.MineralReception;
 using SGC.InterfaceServices.CM.MineralReception;
 using System;
 using System.Collections.Generic;
@@ -24,6 +26,47 @@ namespace SGC.Web.Controllers.CM.MineralReception
             try
             {
                 var result = await this._rumaService.GetAll(id);
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+        // POST api/Ruma/Add/
+        [HttpPost("[action]")]
+        public IActionResult Add([FromBody] Ruma model)
+        {
+            return Ok(
+                _rumaService.Add(model)
+            );
+        }
+
+        // POST api/Ruma/Update/1
+        [HttpPut("[action]")]
+        public IActionResult Update([FromBody] Ruma model)
+        {
+            return Ok(
+                _rumaService.Update(model)
+            );
+        }
+
+        // DELETE api/Ruma/Delete/{}
+        [HttpDelete("[action]")]
+        public IActionResult Delete([FromBody] JObject obj)
+        {
+            return Ok(
+                _rumaService.Delete(obj)
+            );
+        }
+
+        // GET: api/Ruma/Search/{}
+        [HttpPost("[action]")]
+        public async Task<IActionResult> Search(JObject obj)
+        {
+            try
+            {
+                var result = await this._rumaService.Search(obj);
                 return Ok(result);
             }
             catch (Exception ex)
